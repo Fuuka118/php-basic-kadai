c14105d7a20fa5c655d3d6a96ee35790efddd038
fuuka118 <156281581+Fuuka118@users.noreply.github.com>
2024-06-19 02:44:28 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+  </head>
+
+  <body>
+    <?php
+
+    $food = ["name" => "onion", "price" => 200, "weight" => 160];
+
+    print_r($food);
+
+    ?>
+
+  </body>
+
+</html>
