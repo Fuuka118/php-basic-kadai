a02e823fa29b5cff2aa3a69d3cdb4052f60cd7c4
fuuka118 <156281581+Fuuka118@users.noreply.github.com>
2024-09-04 18:11:06 +0900

    Add files via upload

---
+++ kadai_web_app/confirm.php
@@ -0,0 +1,87 @@
+<?php
+
+session_start();
+
+$name = $_POST['user_name'];
+$age = $_POST['user_age'];
+$department = $_POST['department'];
+
+$errors = [];
+
+if (empty($name)){
+    $errors[] = 'お名前を入力してください。';
+}
+
+if (empty($age) ) {
+    $errors[] = '年齢を入力してください。';
+} elseif (!is_numeric($age) ) {
+    $errors[] = '年齢には数値を入力してください。';
+}
+
+
+if (empty($errors)) {
+    // セッション変数を保存
+    $_SESSION['name'] = $name;
+    $_SESSION['age'] = $age;
+    $_SESSION['department'] = $department;
+
+    // クッキーを登録（有効期限は1時間）
+    setcookie('name', $name, time() + 3600 );
+    setcookie('age', $age, time() + 3600 );
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <h2>入力内容をご確認ください。</h2>
+    <p>問題なければ「確定」、修正する場合は「キャンセル」をクリックしてください。</p>
+
+    <table border="1">
+        <tr>
+            <th>項目</th>
+            <th>入力内容</th>
+        </tr>
+        <tr>
+            <td>社員名</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>年齢</td>
+            <td><?php echo $age; ?></td>
+        </tr>
+        <tr>
+            <td>所属部署</td>
+            <td><?php echo $department; ?></td>
+        </tr>
+    </table>
+
+    <p>
+        <button id="confirm-btn" onclick="location.href='complete.php';">確定</button>
+        <button id="cancel-btn" onclick="history.back();">キャンセル</button>
+    </p>
+
+    <?php
+    // 入力項目にエラーがある場合の処理(エラーメッセージの出力と、確定ボタンの無効化)
+    if(!empty($errors)){
+        
+        foreach ($errors as $error) {
+            echo '<font color="red">' . $error . '</font>' . '<br>';
+        }
+
+        echo '<script> document.getElementById("confirm-btn").disabled = true; </script>';
+    }
+
+    ?>
+
+</body>
+
+</html>
